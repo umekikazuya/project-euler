f7b693e12397c5a064e9e3003f9c596f656b17d1
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-06-07 19:01:44 +0900

    Add main.go for problem 002 solution

--- problems/002/main.go
+++ problems/002/main.go
@@ -1,31 +1,18 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// フィボナッチ数列を取得
-func getFibonacci(below int) []int {
-	fibonacci := []int{1, 2}
-	for {
-		// Belowより末尾の数字が大きい場合はループ終了, 末尾の数字を削除後、配列を返却
-		if below < fibonacci[len(fibonacci)-1] {
-			return fibonacci[:len(fibonacci)-1]
-		}
-		// 配列の最後と最後から1つ前を加算したものを配列の末尾にAppend
-		fibonacci = append(fibonacci, fibonacci[len(fibonacci)-1]+fibonacci[len(fibonacci)-2])
-		continue
-	}
-}
-
+// だいぶ前だったから忘れたので二回目
 func main() {
-	// 偶数のみ
+	fibonacciNumbers := []int{1, 1}
+	result := 0
 	sum := 0
-	for _, v := range getFibonacci(4000000) {
-		if v%2 != 0 {
-			continue
+	for sum < 4000000 {
+		sum = fibonacciNumbers[len(fibonacciNumbers)-1] + fibonacciNumbers[len(fibonacciNumbers)-2]
+		if sum%2 == 0 {
+			result += sum
 		}
-		sum += v
+		fibonacciNumbers = append(fibonacciNumbers, sum)
 	}
-	fmt.Println(sum)
+	fmt.Println(result)
 }
